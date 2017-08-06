33d2d856c620ca3c99aed225b4b915dbf7708fc8
ChiLi_Lin <904981663@163.com>
2017-08-06 23:37:47 +0800

    Add ProceduralTextureGeneration

---
+++ Assets/Scripts/Chapter10/ProceduralTextureGeneration.cs
@@ -0,0 +1,171 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// <summary>
+/// 程序纹理生成
+/// </summary>
+[ExecuteInEditMode]
+public class ProceduralTextureGeneration : MonoBehaviour
+{
+
+    public Material material = null;
+
+    #region 材质属性面板设置
+    [SerializeField, SetProperty("textureWidth")]
+    private int m_textureWidth = 512;
+    public int textureWidth
+    {
+        get
+        {
+            return m_textureWidth;
+        }
+        set
+        {
+            m_textureWidth = value;
+            _UpdateMaterial();
+        }
+    }
+
+    [SerializeField, SetProperty("backgroundColor")]
+    private Color m_backgroundColor = Color.white;
+    public Color backgroundColor
+    {
+        get
+        {
+            return m_backgroundColor;
+        }
+        set
+        {
+            m_backgroundColor = value;
+            _UpdateMaterial();
+        }
+    }
+
+    [SerializeField, SetProperty("circleColor")]
+    private Color m_circleColor = Color.yellow;
+    public Color circleColor
+    {
+        get
+        {
+            return m_circleColor;
+        }
+        set
+        {
+            m_circleColor = value;
+            _UpdateMaterial();
+        }
+    }
+
+    [SerializeField, SetProperty("blurFactor")]
+    private float m_blurFactor = 2.0f;
+    public float blurFactor
+    {
+        get
+        {
+            return m_blurFactor;
+        }
+        set
+        {
+            m_blurFactor = value;
+            _UpdateMaterial();
+        }
+    }
+    #endregion
+
+    private Texture2D m_generatedTexture = null;        // 保存生成的程序纹理
+
+    /// <summary>
+    /// 初始化材质
+    /// </summary>
+    void Start()
+    {
+        if (material == null)
+        {
+            Renderer renderer = gameObject.GetComponent<Renderer>();
+            if (renderer == null)
+            {
+                Debug.LogWarning("Cannot find a renderer.");
+                return;
+            }
+
+            material = renderer.sharedMaterial;
+        }
+
+        _UpdateMaterial();
+    }
+
+    /// <summary>
+    /// 更新材质
+    /// </summary>
+    private void _UpdateMaterial()
+    {
+        if (material != null)
+        {
+            m_generatedTexture = _GenerateProceduralTexture();
+            material.SetTexture("_MainTex", m_generatedTexture);
+        }
+    }
+    
+    /// <summary>
+    /// 混合插值颜色
+    /// </summary>
+    /// <param name="color0">颜色1</param>
+    /// <param name="color1">颜色2</param>
+    /// <param name="mixFactor">混合因子</param>
+    /// <returns></returns>
+    private Color _MixColor(Color color0, Color color1, float mixFactor)
+    {
+        Color mixColor = Color.white;
+        mixColor.r = Mathf.Lerp(color0.r, color1.r, mixFactor);
+        mixColor.g = Mathf.Lerp(color0.g, color1.g, mixFactor);
+        mixColor.b = Mathf.Lerp(color0.b, color1.b, mixFactor);
+        mixColor.a = Mathf.Lerp(color0.a, color1.a, mixFactor);
+        return mixColor;
+    }
+
+    /// <summary>
+    /// 生成程序纹理
+    /// </summary>
+    /// <returns>纹理</returns>
+    private Texture2D _GenerateProceduralTexture()
+    {
+        Texture2D proceduralTexture = new Texture2D(textureWidth, textureWidth);
+
+        float circleInterval = textureWidth / 4.0f;             // 圆于圆之间的间距
+        float radius = textureWidth / 10.0f;                    // 圆的半径
+        float edgeBlur = 1.0f / blurFactor;                     // 模糊系数
+
+        for (int w = 0; w < textureWidth; w++)
+        {
+            for (int h = 0; h < textureWidth; h++)
+            {
+                Color pixel = backgroundColor;                  // 背景颜色初始化
+
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        // 计算每个圆的圆心位置
+                        Vector2 circleCenter = new Vector2(circleInterval * (i + 1), circleInterval * (j + 1));
+
+                        // 当前像素与圆心的距离
+                        float dist = Vector2.Distance(new Vector2(w, h), circleCenter) - radius;
+
+                        // 模糊圆的边界
+                        Color color = _MixColor(circleColor, new Color(pixel.r, pixel.g, pixel.b, 0.0f), Mathf.SmoothStep(0f, 1.0f, dist * edgeBlur));
+
+                        // 与之前的颜色混合
+                        pixel = _MixColor(pixel, color, color.a);
+                    }
+                }
+
+                proceduralTexture.SetPixel(w, h, pixel);
+            }
+        }
+
+        proceduralTexture.Apply();
+
+        return proceduralTexture;
+    }
+}
---
+++ Assets/Shaders/Chapter 10/GlassRefraction.shader
@@ -0,0 +1,103 @@
+﻿
+// 玻璃纹理模拟
+Shader "Custom/Chapter 10/Glass Refraction" {
+	Properties {
+		_MainTex ("Main Tex", 2D) = "white" {}
+		_BumpMap ("Normal Map", 2D) = "bump" {}						// 法线纹理
+		_Cubemap ("Environment Cubemap", Cube) = "_Skybox" {}		// 反射纹理
+		_Distortion ("Distortion", Range(0, 100)) = 10				// 折射扭曲程度
+		_RefractAmount ("Refract Amount", Range(0.0, 1.0)) = 1.0	// 控制折射程度（0:只反射，1：只折射）
+	}
+	SubShader {
+		// 在所有不透明物体渲染后再渲染。
+		Tags { "Queue"="Transparent" "RenderType"="Opaque" }
+		
+		// 抓取屏幕图像的Pass，存入到纹理变量中。
+		GrabPass { "_RefractionTex" }
+		
+		Pass {		
+			CGPROGRAM
+			
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+			
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			sampler2D _BumpMap;
+			float4 _BumpMap_ST;
+			samplerCUBE _Cubemap;
+			float _Distortion;
+			fixed _RefractAmount;
+			sampler2D _RefractionTex;			// 对应GrabPass指定的纹理名
+			float4 _RefractionTex_TexelSize;	// 纹理大小。如256x512的纹理，值为（1/256, 1/512）
+
+			
+			struct a2v {
+				float4 vertex : POSITION;
+				float3 normal : NORMAL;
+				float4 tangent : TANGENT; 
+				float2 texcoord: TEXCOORD0;
+			};
+			
+			struct v2f {
+				float4 pos : SV_POSITION;
+				float4 scrPos : TEXCOORD0;
+				float4 uv : TEXCOORD1;
+				float4 TtoW0 : TEXCOORD2;  
+			    float4 TtoW1 : TEXCOORD3;  
+			    float4 TtoW2 : TEXCOORD4; 
+			};
+			
+			v2f vert (a2v v) {
+				v2f o;
+				o.pos = UnityObjectToClipPos(v.vertex);
+				
+				o.scrPos = ComputeGrabScreenPos(o.pos);			// 计算抓取的屏幕图像采样坐标
+				
+				o.uv.xy = TRANSFORM_TEX(v.texcoord, _MainTex);	// 计算采样坐标
+				o.uv.zw = TRANSFORM_TEX(v.texcoord, _BumpMap);
+				
+				float3 worldPos = mul(unity_ObjectToWorld, v.vertex).xyz;  
+				fixed3 worldNormal = UnityObjectToWorldNormal(v.normal);  
+				fixed3 worldTangent = UnityObjectToWorldDir(v.tangent.xyz);  
+				fixed3 worldBinormal = cross(worldNormal, worldTangent) * v.tangent.w; 
+				
+				// 把法线方向从切线空间转到世界空间
+				o.TtoW0 = float4(worldTangent.x, worldBinormal.x, worldNormal.x, worldPos.x);  
+				o.TtoW1 = float4(worldTangent.y, worldBinormal.y, worldNormal.y, worldPos.y);  
+				o.TtoW2 = float4(worldTangent.z, worldBinormal.z, worldNormal.z, worldPos.z);  
+				
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target {		
+				float3 worldPos = float3(i.TtoW0.w, i.TtoW1.w, i.TtoW2.w);
+				fixed3 worldViewDir = normalize(UnityWorldSpaceViewDir(worldPos));
+				
+				// 获取切线空间下的法线方向
+				fixed3 bump = UnpackNormal(tex2D(_BumpMap, i.uv.zw));	
+				
+				// 对屏幕图像采样坐标进行偏移，模拟折射
+				float2 offset = bump.xy * _Distortion * _RefractionTex_TexelSize.xy;
+				i.scrPos.xy = offset * i.scrPos.z + i.scrPos.xy;
+				fixed3 refrCol = tex2D(_RefractionTex, i.scrPos.xy/i.scrPos.w).rgb;		// 透射除法 4.9.3
+				
+				// 法线再转到世界空间
+				bump = normalize(half3(dot(i.TtoW0.xyz, bump), dot(i.TtoW1.xyz, bump), dot(i.TtoW2.xyz, bump)));
+				fixed3 reflDir = reflect(-worldViewDir, bump);
+				fixed4 texColor = tex2D(_MainTex, i.uv.xy);
+				fixed3 reflCol = texCUBE(_Cubemap, reflDir).rgb * texColor.rgb;
+				
+				fixed3 finalColor = reflCol * (1 - _RefractAmount) + refrCol * _RefractAmount;
+				
+				return fixed4(finalColor, 1);
+			}
+			
+			ENDCG
+		}
+	}
+	
+	FallBack "Diffuse"
+}
