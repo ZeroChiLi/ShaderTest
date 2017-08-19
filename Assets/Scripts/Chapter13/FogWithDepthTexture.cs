561939e0cbbf1fc60727bf32af7bd05e7237b309
ChiLi_Lin <904981663@163.com>
2017-08-19 23:39:52 +0800

    Add FogWithDepthTexture

---
+++ Assets/Scripts/Chapter13/FogWithDepthTexture.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 雾效
+/// </summary>
+public class FogWithDepthTexture : PostEffectsBase {
+
+	public Shader fogShader;
+	private Material fogMaterial = null;
+
+	public Material material {  
+		get {
+			fogMaterial = CheckShaderAndCreateMaterial(fogShader, fogMaterial);
+			return fogMaterial;
+		}  
+	}
+
+	private Camera myCamera;
+	public Camera camera {
+		get {
+			if (myCamera == null) {
+				myCamera = GetComponent<Camera>();
+			}
+			return myCamera;
+		}
+	}
+
+	private Transform myCameraTransform;
+	public Transform cameraTransform {
+		get {
+			if (myCameraTransform == null) {
+				myCameraTransform = camera.transform;
+			}
+
+			return myCameraTransform;
+		}
+	}
+
+	[Range(0.0f, 3.0f)]
+	public float fogDensity = 1.0f;         // 雾的密度
+
+	public Color fogColor = Color.white;    // 雾的颜色
+
+	public float fogStart = 0.0f;           // 起始高度
+	public float fogEnd = 2.0f;             // 终止高度
+
+	void OnEnable() {
+		camera.depthTextureMode |= DepthTextureMode.Depth;
+	}
+	
+	void OnRenderImage (RenderTexture src, RenderTexture dest) {
+		if (material != null) {
+			Matrix4x4 frustumCorners = Matrix4x4.identity;
+
+			float fov = camera.fieldOfView;         // 竖直方向视角范围（角度）
+			float near = camera.nearClipPlane;      // 近平面距离 
+			float aspect = camera.aspect;           // 宽高比
+
+            // 计算四个角对应的向量，存到frustumCorners
+			float halfHeight = near * Mathf.Tan(fov * 0.5f * Mathf.Deg2Rad);
+			Vector3 toRight = cameraTransform.right * halfHeight * aspect;
+			Vector3 toTop = cameraTransform.up * halfHeight;
+
+			Vector3 topLeft = cameraTransform.forward * near + toTop - toRight;
+			float scale = topLeft.magnitude / near;
+
+			topLeft.Normalize();
+			topLeft *= scale;
+
+			Vector3 topRight = cameraTransform.forward * near + toRight + toTop;
+			topRight.Normalize();
+			topRight *= scale;
+
+			Vector3 bottomLeft = cameraTransform.forward * near - toTop - toRight;
+			bottomLeft.Normalize();
+			bottomLeft *= scale;
+
+			Vector3 bottomRight = cameraTransform.forward * near + toRight - toTop;
+			bottomRight.Normalize();
+			bottomRight *= scale;
+
+			frustumCorners.SetRow(0, bottomLeft);
+			frustumCorners.SetRow(1, bottomRight);
+			frustumCorners.SetRow(2, topRight);
+			frustumCorners.SetRow(3, topLeft);
+
+			material.SetMatrix("_FrustumCornersRay", frustumCorners);
+
+			material.SetFloat("_FogDensity", fogDensity);
+			material.SetColor("_FogColor", fogColor);
+			material.SetFloat("_FogStart", fogStart);
+			material.SetFloat("_FogEnd", fogEnd);
+
+			Graphics.Blit (src, dest, material);
+		} else {
+			Graphics.Blit(src, dest);
+		}
+	}
+}
